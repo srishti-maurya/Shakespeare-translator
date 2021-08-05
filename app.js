030ee6faa0c80356eb0b86a0c7b4b359a99192ac
srishti-maurya <srishti22maurya@gmail.com>
2021-08-05 13:01:22 +0530

    translator

---
+++ app.js
@@ -0,0 +1,26 @@
+var inputTranslate = document.querySelector("#translate-input");
+var btnTranslate = document.querySelector("#translate-btn");
+var outputTranslate = document.querySelector("#translate-output");
+var UrlServer = "https://api.funtranslations.com/translate/shakespeare.json"
+
+function buildURL(text){
+    return UrlServer+"?"+"text="+text
+ }
+ 
+function handleError(err){
+    console.log("an error occured",err);
+    alert("There is something wrong with the server! Please try again later")
+}
+
+function clickHandler() {
+  var textInput = inputTranslate.value;
+  fetch(buildURL(textInput))
+    .then(res=>res.json())
+    .then(json =>{
+        var translatedText = json.contents.translated;
+        outputTranslate.innerHTML = translatedText;
+    })
+    .catch(handleError)
+}
+
+btnTranslate.addEventListener("click", clickHandler);
